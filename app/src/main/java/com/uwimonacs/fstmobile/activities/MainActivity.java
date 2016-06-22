7afd3e836ac8c4eac0aa792120749725dbec50f7
Brandon Hinds <brandonhinds13@gmail.com>
2016-06-22 14:29:33 -0400

    Added intent to MainActivity

--- app/src/main/java/com/uwimonacs/fstmobile/activities/MainActivity.java
+++ app/src/main/java/com/uwimonacs/fstmobile/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.uwimonacs.fstmobile.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
