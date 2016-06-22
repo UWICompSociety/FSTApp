2de071f1fbdd5667bb1625d286e070bfc052889a
Brandon Hinds <brandonhinds13@gmail.com>
2016-06-22 11:18:51 -0400

    Update FAQActivity.java

--- app/src/main/java/com/uwimonacs/fstmobile/activities/FAQActivity.java
+++ app/src/main/java/com/uwimonacs/fstmobile/activities/FAQActivity.java
@@ -1,15 +1,21 @@
 package com.uwimonacs.fstmobile.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.uwimonacs.fstmobile.R;
+import com.uwimonacs.fstmobile.adapters.FaqListAdapter;
+import com.uwimonacs.fstmobile.models.FAQ;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FAQActivity extends AppCompatActivity {
+
+    private List<FAQ> faqs = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +24,18 @@ public class FAQActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        RecyclerView faqList = (RecyclerView)findViewById(R.id.faqlist);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        faqList.setLayoutManager(llm);
+
+        faqs.add(new FAQ(1,"How Many Credits Do I Need To Graduate?", "A Total of 93 Credits"));
+        faqs.add(new FAQ(2,"Where is FST Dean's Office?", "Along The Spine"));
+        faqs.add(new FAQ(3,"When does Copy-Works Open?", "8:30 AM Mondays - Friday"));
+        faqs.add(new FAQ(4,"Where Can I Buy Books?", "At The Book Store"));
+        faqs.add(new FAQ(5,"Where Can I Find The Computing Society?", "Computing Lecture Theatre"));
+        faqs.add(new FAQ(6,"How Many First Year Computing Courses Are There?", "There are 5 Of Them"));
+
+        FaqListAdapter adapter = new FaqListAdapter(faqs);
+        faqList.setAdapter(adapter);
     }
 }
