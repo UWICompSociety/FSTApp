a1d79bb5e653cc62c5873ec603990a714f8a0fbb
vwainne <vwainne@gmail.com>
2016-06-24 09:55:01 -0500

    Create News Model

---
+++ app/src/main/java/com/uwimonacs/fstmobile/models/Story.java
@@ -0,0 +1,67 @@
+/**
+ * @param imageId
+ * @param title
+ * @param description
+ * @param storyDetail
+ */
+
+public class Story{
+	//Variable Declarations
+	int imageId; //differentiates each news item - do we need an id for each image?
+	String title; //Headline
+	String description; //Short excerpt of story
+	String storyDetail; //Actual news story
+
+	//Constructor Method
+	public Story(int id, String title, String description, String story){
+		this.imageId = id;
+		this.title = title;
+		this.description = description;
+		this.storyDetail = story;
+	}
+
+	/*Getters*/
+	public String getTitle(){
+		return title;
+	}
+
+	public String getDescription(){
+		return description;
+	}
+
+	public String getStory(){
+		return storyDetail;
+	}
+
+	public int getImageId(){
+		return imageId;
+	}
+
+	/*Setters*/
+	public void setTitle(String newTitle){
+		title = newTitle;
+	}
+
+	public void setDescription(String newDescription){
+		description = newDescription;
+	}
+
+	public void setStory(String newStoryDetail){
+		storyDetail = newStoryDetail;
+	}
+
+	public void setImageId(int newFeatureImage){
+		imageId = newFeatureImage;
+	}
+}
+
+private List<Story> news;
+
+/*Array list with three news items*/
+private void placeHolder(){
+	news = new ArrayList<>();
+	news.add(new Story(R.drawable.s1,"Title1","Description1","Detail1"));
+	news.add(new Story(R.drawable.s2,"Title2","Description2","Detail2"));
+	news.add(new Story(R.drawable.s3,"Title3","Description3","Detail3"));
+	news.add(new Story(R.drawable.s4,"Title4","Description4","Detail4"));
+} 
