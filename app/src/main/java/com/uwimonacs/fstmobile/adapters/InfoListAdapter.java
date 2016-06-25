5e436e5069fc7053742b0d93aeeb1a706685ed96
stonecoder19 <generalms@hotmail.com>
2016-06-25 13:46:34 -0500

    Added Info Fragment

---
+++ app/src/main/java/com/uwimonacs/fstmobile/adapters/InfoListAdapter.java
@@ -0,0 +1,93 @@
+package com.uwimonacs.fstmobile.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.identifier.AdvertisingIdClient;
+import com.uwimonacs.fstmobile.R;
+import com.uwimonacs.fstmobile.activities.FAQActivity;
+import com.uwimonacs.fstmobile.activities.VideoListActivity;
+
+/**
+ * Created by Matthew on 6/25/2016.
+ */
+public class InfoListAdapter extends RecyclerView.Adapter<InfoListAdapter.InfoViewHolder> {
+
+
+    public static class InfoViewHolder extends RecyclerView.ViewHolder
+    {
+        TextView infoTitle; //reference to the title text
+        ImageView infoImage; //reference to the info image
+
+        public InfoViewHolder(View itemView) {
+            super(itemView);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int pos = InfoViewHolder.this.getAdapterPosition(); //position of element in list
+                    switch(pos)
+                    {
+                        case 0:
+                            Intent faqIntent = new Intent(v.getContext(),FAQActivity.class);
+                            v.getContext().startActivity(faqIntent); //starting FAQ activity
+                            break;
+                        case 1:
+                            Intent videoIntent = new Intent(v.getContext(),VideoListActivity.class);
+                            v.getContext().startActivity(videoIntent); //starting video activity
+                            break;
+                        case 2:
+                            //Scholarship Actvity
+                            break;
+                    }
+                }
+            });
+
+            infoTitle = (TextView)itemView.findViewById(R.id.info_title);
+            infoImage = (ImageView) itemView.findViewById(R.id.image_info);
+        }
+    }
+
+    String[] titles;
+    int[] images;
+    Context ctxt;
+
+    public InfoListAdapter(Context ctxt,String[] titles,int[] images)
+    {
+        this.ctxt = ctxt;
+        this.titles = titles;
+        this.images = images;
+
+    }
+
+
+    @Override
+    public InfoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_info_item,parent,false); //inflate view with layout file
+        final InfoViewHolder infoViewHolder = new InfoViewHolder(view);
+
+
+
+        return infoViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(InfoViewHolder holder, int position) {
+
+        //setting values
+        holder.infoTitle.setText(titles[position]);
+        holder.infoImage.setImageResource(images[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return titles.length; //number of elements in list
+    }
+}
--- app/src/main/java/com/uwimonacs/fstmobile/fragments/InfoFragment.java
+++ app/src/main/java/com/uwimonacs/fstmobile/fragments/InfoFragment.java
@@ -3,11 +3,14 @@ package com.uwimonacs.fstmobile.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.uwimonacs.fstmobile.R;
+import com.uwimonacs.fstmobile.adapters.InfoListAdapter;
 
 /**
  * Created by Matthew on 6/20/2016.
@@ -16,6 +19,10 @@ public class InfoFragment extends Fragment {
 
 
     private View view;
+    private RecyclerView infoRecView;
+    private InfoListAdapter infoListAdapter;
+    String[] titles = {"FAQ","Videos","Scholarships & Busaries"}; //list of titles for info  fragment
+    int[] images = {R.drawable.faqimage,R.drawable.video,R.drawable.scholarship}; //list of images for info fragment
 
 
     public InfoFragment()
@@ -27,6 +34,17 @@ public class InfoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.frag_info,container,false); //inflates the layout for the view
+
+        infoRecView = (RecyclerView)view.findViewById(R.id.listInfo); //referencing recycler view
+        infoListAdapter = new InfoListAdapter(getActivity(),titles,images);  //creating the list adapter
+
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity()); //creating layout manager necessary for recycler view to function
+
+        infoRecView.setLayoutManager(llm);
+        infoRecView.setHasFixedSize(true);
+
+        infoRecView.setAdapter(infoListAdapter); //setting the adapter of the list
+
 
         return view;
     }
