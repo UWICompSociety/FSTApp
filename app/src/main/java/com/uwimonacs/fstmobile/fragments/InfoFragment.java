33599ea53c4ae2db5fbd4e9be4ff54109996248d
Romario Maxwell <romario.maxwell@gmail.com>
2016-06-25 15:08:09 -0500

    Rename image resource
    The project could not be compiled because there was a capital letter in the name of an image resource, which was also different from its sole reference in a source file. I have no idea why Android imposes this restriction.

--- app/src/main/java/com/uwimonacs/fstmobile/fragments/InfoFragment.java
+++ app/src/main/java/com/uwimonacs/fstmobile/fragments/InfoFragment.java
@@ -12,22 +12,18 @@ import android.view.ViewGroup;
 import com.uwimonacs.fstmobile.R;
 import com.uwimonacs.fstmobile.adapters.InfoListAdapter;
 
-/**
- * Created by Matthew on 6/20/2016.
- */
+@SuppressWarnings("FieldCanBeLocal")
 public class InfoFragment extends Fragment {
-
-
     private View view;
     private RecyclerView infoRecView;
     private InfoListAdapter infoListAdapter;
-    String[] titles = {"FAQ","Videos","Scholarships & Busaries"}; //list of titles for info  fragment
-    int[] images = {R.drawable.faqimage,R.drawable.video,R.drawable.scholarship}; //list of images for info fragment
 
+    String[] titles = {"FAQ", "Videos", "Scholarships & Bursaries"}; //list of titles for info  fragment
+    int[] images = {R.drawable.faq_image, R.drawable.video, R.drawable.scholarship}; //list of images for info fragment
 
     public InfoFragment()
     {
-
+        /* required empty constructor */
     }
 
     @Nullable
@@ -36,7 +32,7 @@ public class InfoFragment extends Fragment {
         view = inflater.inflate(R.layout.frag_info,container,false); //inflates the layout for the view
 
         infoRecView = (RecyclerView)view.findViewById(R.id.listInfo); //referencing recycler view
-        infoListAdapter = new InfoListAdapter(getActivity(),titles,images);  //creating the list adapter
+        infoListAdapter = new InfoListAdapter(getActivity(), titles, images);  //creating the list adapter
 
         LinearLayoutManager llm = new LinearLayoutManager(getActivity()); //creating layout manager necessary for recycler view to function
 
@@ -44,7 +40,6 @@ public class InfoFragment extends Fragment {
         infoRecView.setHasFixedSize(true);
 
         infoRecView.setAdapter(infoListAdapter); //setting the adapter of the list
-
 
         return view;
     }
