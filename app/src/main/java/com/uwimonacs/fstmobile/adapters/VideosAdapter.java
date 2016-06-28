c1345d4f4e4f37a25841a6341ddc2cf13e348730
sultanofcardio <kevin.lindsay65@gmail.com>
2016-06-28 01:01:46 -0500

    Added string relevant to the video activities

---
+++ app/src/main/java/com/uwimonacs/fstmobile/adapters/VideosAdapter.java
@@ -0,0 +1,7 @@
+package com.uwimonacs.fstmobile.adapters;
+
+/**
+ * Created by sultanofcardio on 6/27/16.
+ */
+public class VideosAdapter {
+}
---
+++ app/src/main/java/com/uwimonacs/fstmobile/models/VideoViewHolder.java
@@ -0,0 +1,7 @@
+package com.uwimonacs.fstmobile.models;
+
+/**
+ * Created by sultanofcardio on 6/27/16.
+ */
+public class VideoViewHolder {
+}
