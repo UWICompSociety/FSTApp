4a22535d7e89484dc1d950df464c4cd6766ba3b4
Romario Maxwell <romario.maxwell@gmail.com>
2016-07-28 13:11:01 -0500

    Improve appearance of Info related items

--- app/src/main/java/com/uwimonacs/fstmobile/activities/ExpandableScholarshipActivity.java
+++ app/src/main/java/com/uwimonacs/fstmobile/activities/ExpandableScholarshipActivity.java
@@ -30,7 +30,6 @@ import it.gmariotti.cardslib.library.recyclerview.internal.CardArrayRecyclerView
 import it.gmariotti.cardslib.library.recyclerview.view.CardRecyclerView;
 
 public class ExpandableScholarshipActivity extends AppCompatActivity {
-
     private List<Scholarship> schols;
     private String url = Constants.SCHOL_URL;
     private List<Card> cards;
@@ -44,7 +43,6 @@ public class ExpandableScholarshipActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //enable back button in the toolbar
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
@@ -55,10 +53,8 @@ public class ExpandableScholarshipActivity extends AppCompatActivity {
 
         CardRecyclerView mRecyclerView = (CardRecyclerView) findViewById(R.id.card_recyclerview);
 
-        //Initializes adapter
         mCardArrayAdapter = new ExpandableScholarshipAdapter(this, cards);
 
-        //Initializes and sets layout manager
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         if (mRecyclerView != null) {
             mRecyclerView.setAdapter(mCardArrayAdapter);
@@ -75,8 +71,7 @@ public class ExpandableScholarshipActivity extends AppCompatActivity {
     public static Drawable LoadImageFromWebOperations(String url) {
         try {
             InputStream is = (InputStream) new URL(url).getContent();
-            Drawable d = Drawable.createFromStream(is, "src name");
-            return d;
+            return Drawable.createFromStream(is, "src name");
         } catch (Exception e) {
             return null;
         }
@@ -97,7 +92,7 @@ public class ExpandableScholarshipActivity extends AppCompatActivity {
             MaterialLargeImageCard card = new MaterialLargeImageCard(this);
 
             //Set relevant data for the card
-            card.setDrawableIdCardThumbnail(R.drawable.scholarship);
+            card.setDrawableIdCardThumbnail(R.drawable.ic_school_black_24dp);
             //card.setDrawableIdCardThumbnail(LoadImageFromWebOperations(schols.get(i).getImage()));
             card.setTitle(schols.get(i).getTitle());
             card.setSubTitle(schols.get(i).getDescription());
@@ -130,7 +125,8 @@ public class ExpandableScholarshipActivity extends AppCompatActivity {
 
         @Override
         protected void onPreExecute() {
-            Toast.makeText(ctxt, "Loading Scholarships and Bursaries...", Toast.LENGTH_SHORT).show();
+            Toast.makeText(ctxt, "Loading Scholarships and Bursaries...",
+                    Toast.LENGTH_SHORT).show();
         }
 
         @Override
@@ -144,10 +140,10 @@ public class ExpandableScholarshipActivity extends AppCompatActivity {
         protected void onPostExecute(Boolean result) {
             if (result) {
                 initMaterialCards();
-                Toast.makeText(ctxt, "Scholarships loaded successfully",Toast.LENGTH_SHORT).show();
+                Toast.makeText(ctxt, "Scholarships loaded successfully", Toast.LENGTH_SHORT).show();
                 mCardArrayAdapter.updateSchols(cards);
             }else {
-                Toast.makeText(ctxt,"Failed",Toast.LENGTH_SHORT).show();
+                Toast.makeText(ctxt, "Failed", Toast.LENGTH_SHORT).show();
             }
         }
 
--- app/src/main/java/com/uwimonacs/fstmobile/fragments/InfoFragment.java
+++ app/src/main/java/com/uwimonacs/fstmobile/fragments/InfoFragment.java
@@ -18,8 +18,9 @@ public class InfoFragment extends Fragment {
     private RecyclerView infoRecView;
     private InfoListAdapter infoListAdapter;
 
-    String[] titles = {"FAQ", "Videos", "Scholarships & Bursaries","Contacts"}; //list of titles for info  fragment
-    int[] images = {R.drawable.faq_image, R.drawable.video, R.drawable.scholarship,R.drawable.contacts}; //list of images for info fragment
+    String[] titles = {"FAQ", "Videos", "Scholarships & Bursaries", "Contacts"};
+    int[] images = {R.drawable.ic_info_black_24dp, R.drawable.ic_videocam_black_24dp,
+            R.drawable.ic_school_black_24dp, R.drawable.ic_contacts_black_24dp};
 
     public InfoFragment()
     {
@@ -28,18 +29,19 @@ public class InfoFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.frag_info,container,false); //inflates the layout for the view
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.frag_info, container, false);
 
-        infoRecView = (RecyclerView)view.findViewById(R.id.listInfo); //referencing recycler view
-        infoListAdapter = new InfoListAdapter(getActivity(), titles, images);  //creating the list adapter
+        infoRecView = (RecyclerView)view.findViewById(R.id.listInfo);
+        infoListAdapter = new InfoListAdapter(getActivity(), titles, images);
 
-        LinearLayoutManager llm = new LinearLayoutManager(getActivity()); //creating layout manager necessary for recycler view to function
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
 
         infoRecView.setLayoutManager(llm);
         infoRecView.setHasFixedSize(true);
 
-        infoRecView.setAdapter(infoListAdapter); //setting the adapter of the list
+        infoRecView.setAdapter(infoListAdapter);
 
         return view;
     }
