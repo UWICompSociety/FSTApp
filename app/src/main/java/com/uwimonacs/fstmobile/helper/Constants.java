36834b6a75450d6db65ae5677e57b7d28df31f63
Romario Maxwell <romario.maxwell@gmail.com>
2016-07-28 00:58:27 -0500

    Make String literal constants final

--- app/src/main/java/com/uwimonacs/fstmobile/helper/Constants.java
+++ app/src/main/java/com/uwimonacs/fstmobile/helper/Constants.java
@@ -1,13 +1,15 @@
 package com.uwimonacs.fstmobile.helper;
 
-/**
- * Created by Matthew on 7/8/2016.
- */
 public  class Constants {
+    public static final String CONTACTS_URL =
+            "https://infinite-citadel-20637.herokuapp.com/contacts/";
 
-    public static String CONTACTS_URL = "https://infinite-citadel-20637.herokuapp.com/contacts/";
-    public static String NEWS_URL  = "https://infinite-citadel-20637.herokuapp.com/news/";
-    public static String FAQS_URL = "https://infinite-citadel-20637.herokuapp.com/faqs/";
-    public static String SCHOL_URL = "https://infinite-citadel-20637.herokuapp.com/scholarship/";
-    public static String PLACE_URL = "https://infinite-citadel-20637.herokuapp.com/places/";
+    public static final String NEWS_URL  = "https://infinite-citadel-20637.herokuapp.com/news/";
+
+    public static final String FAQS_URL = "https://infinite-citadel-20637.herokuapp.com/faqs/";
+
+    public static final String SCHOL_URL =
+            "https://infinite-citadel-20637.herokuapp.com/scholarship/";
+
+    public static final String PLACE_URL = "https://infinite-citadel-20637.herokuapp.com/places/";
 }
