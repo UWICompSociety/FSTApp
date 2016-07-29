7fc2ce508aa8a0b3851d8ee3b7d73d5b2297571a
Romario Maxwell <romario.maxwell@gmail.com>
2016-07-29 18:53:08 -0500

    Refactor UI from run time to static
    Since the cards of the InfoFragment are really just for navigational purposes (to go to other activities), I didn't see a reason for the use of a RecyclerView and corresponding Adapter for it. These cards are not dynamic and I think this is simpler and doesn't incur the run time overhead of the old way.

--- app/src/main/java/com/uwimonacs/fstmobile/fragments/InfoFragment.java
+++ app/src/main/java/com/uwimonacs/fstmobile/fragments/InfoFragment.java
@@ -1,48 +1,74 @@
 package com.uwimonacs.fstmobile.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.uwimonacs.fstmobile.R;
-import com.uwimonacs.fstmobile.adapters.InfoListAdapter;
+import com.uwimonacs.fstmobile.activities.ContactsActivity;
+import com.uwimonacs.fstmobile.activities.FAQActivity;
+import com.uwimonacs.fstmobile.activities.ScholarshipActivity;
+import com.uwimonacs.fstmobile.activities.VideoListActivity;
+import com.uwimonacs.fstmobile.helper.Connect;
 
-@SuppressWarnings("FieldCanBeLocal")
-public class InfoFragment extends Fragment {
-    private View view;
-    private RecyclerView infoRecView;
-    private InfoListAdapter infoListAdapter;
-
-    String[] titles = {"FAQ", "Videos", "Scholarships & Bursaries", "Contacts"};
-    int[] images = {R.mipmap.ic_faqicon, R.mipmap.ic_video,
-            R.drawable.ic_school_black_24dp, R.mipmap.ic_contacts};
-
-    public InfoFragment()
-    {
-        /* required empty constructor */
-    }
-
+public class InfoFragment extends Fragment
+        implements View.OnClickListener {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.frag_info, container, false);
+        View v = inflater.inflate(R.layout.frag_info, container, false);
 
-        infoRecView = (RecyclerView)view.findViewById(R.id.listInfo);
-        infoListAdapter = new InfoListAdapter(getActivity(), titles, images);
+        CardView card_faq = (CardView) v.findViewById(R.id.faq);
+        CardView card_videos = (CardView) v.findViewById(R.id.videos);
+        CardView card_scholarships = (CardView) v.findViewById(R.id.scholarships);
+        CardView card_contacts = (CardView) v.findViewById(R.id.contacts);
 
-        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+        card_faq.setOnClickListener(this);
+        card_videos.setOnClickListener(this);
+        card_scholarships.setOnClickListener(this);
+        card_contacts.setOnClickListener(this);
 
-        infoRecView.setLayoutManager(llm);
-        infoRecView.setHasFixedSize(true);
-
-        infoRecView.setAdapter(infoListAdapter);
-
-        return view;
+        return v;
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.faq:
+                Intent faqIntent = new Intent(v.getContext(), FAQActivity.class);
+                v.getContext().startActivity(faqIntent);
+                break;
+
+            case R.id.videos:
+                if (new Connect(v.getContext()).isConnected()) {
+                    Intent videoIntent = new Intent(v.getContext(), VideoListActivity.class);
+                    v.getContext().startActivity(videoIntent);
+                } else {
+                    Snackbar.make(v, "Check your Internet connection", Snackbar.LENGTH_SHORT).show();
+                }
+                break;
+
+            case R.id.scholarships:
+                Intent scholarshipIntent = new Intent(v.getContext(), ScholarshipActivity.class);
+                v.getContext().startActivity(scholarshipIntent);
+                break;
+
+            case R.id.contacts:
+                Intent contactIntent = new Intent(v.getContext(), ContactsActivity.class);
+                v.getContext().startActivity(contactIntent);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    public InfoFragment() { /* required empty constructor */ }
 }
