9dbb126bfde93aaac70f809f6ec408400e0cc4f4
Romario Maxwell <romario.maxwell@gmail.com>
2016-08-03 17:55:41 -0500

    Refactor connection utils class

--- app/src/main/java/com/uwimonacs/fstmobile/fragments/NewsFragment.java
+++ app/src/main/java/com/uwimonacs/fstmobile/fragments/NewsFragment.java
@@ -19,7 +19,7 @@ import android.widget.TextView;
 import com.activeandroid.query.Select;
 import com.uwimonacs.fstmobile.R;
 import com.uwimonacs.fstmobile.adapters.NewsListAdapter;
-import com.uwimonacs.fstmobile.helper.Connect;
+import com.uwimonacs.fstmobile.helper.ConnectUtils;
 import com.uwimonacs.fstmobile.helper.Constants;
 import com.uwimonacs.fstmobile.models.News;
 import com.uwimonacs.fstmobile.sync.NewsSync;
@@ -32,7 +32,6 @@ public class NewsFragment extends Fragment implements SwipeRefreshLayout.OnRefre
     private RecyclerView newsListView;
     private List<News> newsItems;
     private SwipeRefreshLayout swipeRefreshLayout;
-    private Connect connect;
     private ImageView img_placeholder;
     private TextView tv_placeholder;
     private ProgressBar progressBar;
@@ -55,8 +54,6 @@ public class NewsFragment extends Fragment implements SwipeRefreshLayout.OnRefre
         initViews(); //initialize the views
 
         setUpSwipeRefresh();
-
-        connect = new Connect(this.getActivity());
 
         getNewsFromDatabase(); // gets news items from the database
 
@@ -110,15 +107,15 @@ public class NewsFragment extends Fragment implements SwipeRefreshLayout.OnRefre
     }
 
     private boolean isConnected() {
-        return connect.isConnected();
+        return ConnectUtils.isConnected(getActivity());
     }
 
-    private boolean hasInternet()
+    private static boolean hasInternet()
     {
         boolean hasInternet;
 
         try {
-            hasInternet = connect.haveInternetConnectivity();
+            hasInternet = ConnectUtils.haveInternetConnectivity();
         } catch(Exception e) {
             hasInternet = false;
         }
