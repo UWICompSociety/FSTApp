891b625c7c3f76e83a25605c1ce938f0fee6e5f9
root <brandonhinds13@gmail.com>
2016-08-15 14:42:36 -0400

    Added runtime permissions for map activity

--- app/src/main/java/com/uwimonacs/fstmobile/activities/MapActivity.java
+++ app/src/main/java/com/uwimonacs/fstmobile/activities/MapActivity.java
@@ -1,7 +1,11 @@
 package com.uwimonacs.fstmobile.activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -49,6 +53,8 @@ public class MapActivity extends AppCompatActivity {
     private HashMap<String, Integer> colors;
     private DirectionsRoute currentRoute;
 
+    private static final int PERMISSIONS_LOCATION = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -84,6 +90,18 @@ public class MapActivity extends AppCompatActivity {
             @Override
             public void onMapReady(MapboxMap mapboxMap) {
                 map = mapboxMap;
+
+                if ((ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) &&
+                        (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) &&
+                        (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) &&
+                        (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_NETWORK_STATE) != PackageManager.PERMISSION_GRANTED)) {
+                    ActivityCompat.requestPermissions(MapActivity.this,
+                            new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                                    Manifest.permission.ACCESS_FINE_LOCATION,
+                                    Manifest.permission.INTERNET,
+                                    Manifest.permission.ACCESS_NETWORK_STATE}, PERMISSIONS_LOCATION);
+                }
+
                 mapboxMap.setMyLocationEnabled(true);
 
                 final String[] locals = location.split(",");
@@ -240,5 +258,19 @@ public class MapActivity extends AppCompatActivity {
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_LOCATION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(MapActivity.this,"Permissions Granted.",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(MapActivity.this,"Permissions Denied, You cannot access map features.",Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
     }
 }
