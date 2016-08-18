0168b93033c6a85ac369a78e014c140e5f28befc
sultanofcardio <kevin.lindsay65@gmail.com>
2016-08-18 02:39:11 -0500

    Modified permission request
    Added a help dialog to explain the permission request

--- app/src/main/java/com/uwimonacs/fstmobile/activities/PermissionsActivity.java
+++ app/src/main/java/com/uwimonacs/fstmobile/activities/PermissionsActivity.java
@@ -1,6 +1,9 @@
 package com.uwimonacs.fstmobile.activities;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
@@ -18,41 +21,64 @@ public class PermissionsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_permissions);
+        if(!checkPermissions())
+            showHelpDialog();
+        else
+            startMapActivity();
+    }
 
-        if ((ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) |
-                (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) |
-                (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) |
-                (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_NETWORK_STATE) != PackageManager.PERMISSION_GRANTED)) {
+    private boolean checkPermissions(){
+        return !((ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) ||
+                (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) ||
+                (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) ||
+                (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_NETWORK_STATE) != PackageManager.PERMISSION_GRANTED));
+    }
 
+    private void requestPermissions(){
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
                             Manifest.permission.ACCESS_FINE_LOCATION,
                             Manifest.permission.INTERNET,
                             Manifest.permission.ACCESS_NETWORK_STATE}, PERMISSIONS_LOCATION);
-        } else {
-            Intent intent = new Intent(this, MapActivity.class);
-            intent.putExtras(getIntent());
-            startActivity(intent);
-            finish();
-        }
+    }
+
+    private void showHelpDialog() {
+        final String message = "We need your permission to access your location so we can give you " +
+                "directions to the room you're looking for. Select OK to proceed.";
+
+        final AlertDialog dialog = new AlertDialog.Builder(this)
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        requestPermissions();
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                })
+                .create();
+        dialog.show();
+    }
+
+    private void startMapActivity(){
+        Intent intent = new Intent(this, MapActivity.class);
+        intent.putExtras(getIntent());
+        startActivity(intent);
+        finish();
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
             case PERMISSIONS_LOCATION:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                    Toast.makeText(this,"Permissions Granted.",Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(this, MapActivity.class);
-                    intent.putExtras(getIntent());
-                    startActivity(intent);
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    startMapActivity();
+                else
                     finish();
-                }
-                else {
-                    //Show dialog explaining permissions
-                    Toast.makeText(this,"Permissions Denied, You cannot access map features.",Toast.LENGTH_SHORT).show();
-                    finish();
-                }
                 break;
         }
     }
