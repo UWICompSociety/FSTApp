dea13e9d1879fd0f8327c30bddf76e081278f5d6
sultanofcardio <kevin.lindsay65@gmail.com>
2016-08-20 11:49:44 -0500

    Moved news title to collapsing toolbar

--- app/src/main/java/com/uwimonacs/fstmobile/activities/NewsDetailActivity.java
+++ app/src/main/java/com/uwimonacs/fstmobile/activities/NewsDetailActivity.java
@@ -1,19 +1,20 @@
 package com.uwimonacs.fstmobile.activities;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.v4.widget.NestedScrollView;
+//import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 import com.uwimonacs.fstmobile.R;
+
+import net.opacapp.multilinecollapsingtoolbar.CollapsingToolbarLayout;
 
 public class NewsDetailActivity extends AppCompatActivity {
     private Toolbar toolbar;
@@ -24,7 +25,6 @@ public class NewsDetailActivity extends AppCompatActivity {
     private String story;
     private String image_url;
     private ImageView newsHeader;
-    private TextView newsTitle;
     private TextView newsStory;
 
     @SuppressWarnings("ConstantConditions")
@@ -41,10 +41,7 @@ public class NewsDetailActivity extends AppCompatActivity {
         appbarTitleBehaviour();
 
         newsStory.setText(story);
-        newsTitle.setText(title);
         Picasso.with(this).load(image_url).into(newsHeader);
-
-        newsTitle.requestFocus(); //Prevents title scrolling offscreen
     }
 
     private void init(){
@@ -56,18 +53,22 @@ public class NewsDetailActivity extends AppCompatActivity {
         story = extras.getString("story");
         image_url = extras.getString("image");
         newsHeader = (ImageView) findViewById(R.id.newsDetail_header);
-        newsTitle = (TextView) findViewById(R.id.newsDetail_Topic);
         newsStory = (TextView) findViewById(R.id.newsStory);
     }
     
     private void appbarTitleBehaviour(){
+        toolbarLayout.setExpandedTitleTextAppearance(R.style.TextAppearance_Title);
+        if(Build.VERSION.SDK_INT < 23)
+            toolbarLayout.setExpandedTitleColor(getResources().getColor(android.R.color.white));
+        else
+            toolbarLayout.setExpandedTitleColor(getColor(android.R.color.white));
         appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
             @Override
             public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
                 if(toolbarLayout.getHeight() + verticalOffset < 2 * ViewCompat.getMinimumHeight(toolbarLayout))
                     toolbarLayout.setTitle("News");
                 else
-                    toolbarLayout.setTitle(" ");
+                    toolbarLayout.setTitle(title);
             }
         });
     }
