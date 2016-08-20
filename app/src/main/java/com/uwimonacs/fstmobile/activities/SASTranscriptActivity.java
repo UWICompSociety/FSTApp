13e4dfd015f223862f3a861d2d1a2aacd05b947b
sultanofcardio <kevin.lindsay65@gmail.com>
2016-08-20 02:50:13 -0500

    Refactored SASTranscriptActivity

--- app/src/main/java/com/uwimonacs/fstmobile/activities/SASTranscriptActivity.java
+++ app/src/main/java/com/uwimonacs/fstmobile/activities/SASTranscriptActivity.java
@@ -4,18 +4,24 @@ import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 import com.uwimonacs.fstmobile.MyApplication;
 import com.uwimonacs.fstmobile.R;
 import com.uwimonacs.fstmobile.adapters.SASTranscriptAdapter;
 import com.uwimonacs.fstmobile.models.SASConfig;
+import com.uwimonacs.fstmobile.models.Transcript;
 
 import uk.co.deanwild.materialshowcaseview.MaterialShowcaseView;
 
@@ -47,10 +53,44 @@ SwipeRefreshLayout.OnRefreshListener{
                     .show();
         }
 
+        Transcript transcript = sasConfig.student.getTranscript();
+        TextView degreeType = (TextView) findViewById(R.id.degree_type),
+                program = (TextView) findViewById(R.id.program),
+                faculty = (TextView) findViewById(R.id.faculty),
+                major = (TextView) findViewById(R.id.major),
+                minor = (TextView) findViewById(R.id.minor);
+
+        String sDegree = "Degree type: " + transcript.getDegree(),
+                sProgram = "Program: " + transcript.getProgram(),
+                sFaculty = "Faculty: " + transcript.getFaculty(),
+                sMajor = "Major: " + transcript.getMajor(),
+                sMinor = "Minor: " + transcript.getMinor();
+
+        degreeType.setText(sDegree);
+        program.setText(sProgram);
+        faculty.setText(sFaculty);
+        major.setText(sMajor);
+        minor.setText(sMinor);
+
         swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.transcript_swiperefresh);
         swipeRefreshLayout.setOnRefreshListener(this);
 
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.app_bar);
+        final CollapsingToolbarLayout toolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapse_toolbar);
+
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if(toolbarLayout.getHeight() + verticalOffset < 2 * ViewCompat.getMinimumHeight(toolbarLayout))
+                    toolbarLayout.setTitle("View your transcript");
+                else
+                    toolbarLayout.setTitle(" ");
+            }
+        });
 
         final RecyclerView institutionCredit = (RecyclerView) findViewById(R.id.institution_credit);
         institutionCredit.setHasFixedSize(true);
