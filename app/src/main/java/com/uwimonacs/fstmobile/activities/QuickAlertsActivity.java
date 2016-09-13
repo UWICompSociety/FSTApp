3398ff8ae72def012d11b84caea8fae70eac714e
sultanofcardio <kevin.lindsay65@gmail.com>
2016-09-13 08:23:13 -0500

    Minor fixes

--- app/src/main/java/com/uwimonacs/fstmobile/activities/QuickAlertsActivity.java
+++ app/src/main/java/com/uwimonacs/fstmobile/activities/QuickAlertsActivity.java
@@ -2,14 +2,22 @@ package com.uwimonacs.fstmobile.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.uwimonacs.fstmobile.R;
 
+@SuppressWarnings("ConstantConditions")
 public class QuickAlertsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quick_alerts);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle(getResources().getString(R.string.title_activity_quick_alerts));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
