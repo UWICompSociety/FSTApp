402f490ea4be8887b5acd6dbce5f0c3a4a3c4eda
sultanofcardio <kevin.lindsay65@gmail.com>
2016-09-13 09:13:06 -0500

    Fixed NewsDetailActivity title

--- app/src/main/java/com/uwimonacs/fstmobile/activities/NewsDetailActivity.java
+++ app/src/main/java/com/uwimonacs/fstmobile/activities/NewsDetailActivity.java
@@ -2,6 +2,7 @@ package com.uwimonacs.fstmobile.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -27,7 +28,9 @@ public class NewsDetailActivity extends AppCompatActivity {
 
         final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("News");
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
         final Bundle extras = getIntent().getExtras();
 
