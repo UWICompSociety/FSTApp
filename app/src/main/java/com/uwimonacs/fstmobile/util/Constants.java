3464dc1ca968bfc7b2c98f6910e46e12fbeb3ae9
sultanofcardio <kevin.lindsay65@gmail.com>
2016-09-15 11:49:28 -0500

    Added alerts URL skeleton

--- app/src/main/java/com/uwimonacs/fstmobile/util/Constants.java
+++ app/src/main/java/com/uwimonacs/fstmobile/util/Constants.java
@@ -22,4 +22,8 @@ public class Constants {
     public static final String EVENTS_URL = HOST_URL + "/events/";
 
 
+    //TODO: Update alerts URL
+    public static final String ALERTS_URL = HOST_URL + "";
+
+
 }
