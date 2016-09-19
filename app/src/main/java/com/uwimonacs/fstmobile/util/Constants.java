f7d93f8645568fd200204e6da1f81c5e5ad61246
Kenroy Gobourne <kevin.lindsay65@gmail.com>
2016-09-18 23:42:21 -0500

    Some housekeeping

--- app/src/main/java/com/uwimonacs/fstmobile/util/Constants.java
+++ app/src/main/java/com/uwimonacs/fstmobile/util/Constants.java
@@ -5,7 +5,6 @@ public class Constants {
 
     public static final String HOST_URL =  "http://fast.mona.uwi.edu";
 
-
     public static final String CONTACTS_URL = HOST_URL + "/contacts/";
 
     public static final String NEWS_URL  = HOST_URL + "/news/";
@@ -16,13 +15,10 @@ public class Constants {
 
     public static final String PLACE_URL = HOST_URL + "/places/";
 
-
     public static final String IMAGES_URL = HOST_URL + "/images/";
 
     public static final String EVENTS_URL = HOST_URL + "/events/";
 
-
-    //TODO: Update alerts URL
     public static final String ALERTS_URL = HOST_URL + "/alerts/";
 
 
